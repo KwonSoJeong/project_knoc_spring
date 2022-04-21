ff14b9ace8bd98be14bdc1876fa42133d7277495
KwonSoJeong <rnjswjd98@gmail.com>
2022-04-22 00:31:01 +0900

    [22.04.22] 클래스, 회원 관련 DAO 수정

--- src/main/java/service/Member_Study_InfoDao.java
+++ src/main/java/service/Member_Study_InfoDao.java
@@ -4,133 +4,95 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import model.Member_Study_Info;
 import util.MyBatisConnection;
+import util.MySqlSessionFactory;
 
+@Component
 public class Member_Study_InfoDao {
 	private final static String ns = "member_study_info.";
 	private static Map<String, Object> map = new HashMap<>();
 	
+	@Autowired
+	MySqlSessionFactory sqlSessionFactory;
+	SqlSession sqlSession;
+	
+	@PostConstruct
+	public void setSqlSession() {
+		this.sqlSession = sqlSessionFactory.sqlmap.openSession();
+	}
+	
 	// 클래스/멘토링/스터디 생성, 신청 시에 info 등록
 	// controller에서 msi 객체에 유저 아이디, 해당 클래스/멘토링/스터디 아이디, 등록자일 시 1/ 참가자일 시 2로 set 하고 전달
 	public void insertInfo(Member_Study_Info msi) {
-		SqlSession sqlSession = MyBatisConnection.getConnection();
-		
 		try {
 			sqlSession.insert(ns + "insertInfo", msi);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			MyBatisConnection.close(sqlSession);
+			sqlSession.commit();
 		}
 
 	}
 	
 	public Member_Study_Info infoOne(String id, String value) {
-		SqlSession sqlSession = MyBatisConnection.getConnection();
 		map.clear();
 		map.put("id", id);
 		map.put("value", value);
-		try {
-			return sqlSession.selectOne(ns + "infoOne", map);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			MyBatisConnection.close(sqlSession);
-		}
 		
-		return null;
+		return sqlSession.selectOne(ns + "infoOne", map);
+		
 	}
 	
 	public List<Member_Study_Info> infoList(String id, String value) {
-		SqlSession sqlSession = MyBatisConnection.getConnection();
 		map.clear();
 		map.put("id", id);
 		map.put("value", value);
 		
-		try {
-			return sqlSession.selectList(ns + "infoList", map);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			MyBatisConnection.close(sqlSession);
-		}
+		return sqlSession.selectList(ns + "infoList", map);
 		
-		return null;
 	}
 	
 	public int nextSeq() {
-		SqlSession sqlSession = MyBatisConnection.getConnection();
 		
-		try {
-			return sqlSession.selectOne(ns + "nextSeq");
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			MyBatisConnection.close(sqlSession);
-		}
-		
-		return 0;
+		return sqlSession.selectOne(ns + "nextSeq");
 
 	}
 	
 	public List<Map<String, Object>> infoClassList(String id) {
-		SqlSession sqlSession = MyBatisConnection.getConnection();
-		
 		map.clear();
 		map.put("table", "classes");
 		map.put("id", "class_id");
 		map.put("value", id);
 		
-		try {
-			return sqlSession.selectList(ns + "infoTitleList", map);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			MyBatisConnection.close(sqlSession);
-		}
+		return sqlSession.selectList(ns + "infoTitleList", map);
 		
-		return null;
 	}
 	
 	public List<Map<String, Object>> infoStudyList(String id) {
-		SqlSession sqlSession = MyBatisConnection.getConnection();
-		
 		map.clear();
 		map.put("table", "study");
 		map.put("id", "study_id");
 		map.put("value", id);
 		
-		try {
-			return sqlSession.selectList(ns + "infoTitleList", map);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			MyBatisConnection.close(sqlSession);
-		}
+		return sqlSession.selectList(ns + "infoTitleList", map);
 		
-		return null;
 	}
 	
 	public List<Map<String, Object>> infoMentoringList(String id) {
-		SqlSession sqlSession = MyBatisConnection.getConnection();
-		
 		map.clear();
 		map.put("table", "mentoring");
 		map.put("id", "mentoring_id");
 		map.put("value", id);
 		
-		try {
-			return sqlSession.selectList(ns + "infoTitleList", map);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			MyBatisConnection.close(sqlSession);
-		}
+		return sqlSession.selectList(ns + "infoTitleList", map);
 		
-		return null;
 	}
 	
 	/* DAO 테스트 코드 
--- src/main/java/service/WebChatDao.java
+++ src/main/java/service/WebChatDao.java
@@ -4,71 +4,60 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import model.WebChat;
 import util.MyBatisConnection;
+import util.MySqlSessionFactory;
 
+@Component
 public class WebChatDao {
 	private final static String ns = "webchat.";
 	private Map<String, Object> map = new HashMap<>();
 	
+	@Autowired
+	MySqlSessionFactory sqlSessionFactory;
+	SqlSession sqlSession;
+	
+	@PostConstruct
+	public void setSqlSession() {
+		this.sqlSession = sqlSessionFactory.sqlmap.openSession();
+	}
+	
 	// 채팅 메세지에 새 번호 붙이기 위해 시퀀스 번호 불러옴
 	public int nextSeq() {
-		SqlSession sqlSession = MyBatisConnection.getConnection();
 		
-		try {
-			return sqlSession.selectOne(ns + "nextSeq");
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			MyBatisConnection.close(sqlSession);
-		}
+		return sqlSession.selectOne(ns + "nextSeq");
 		
-		return 0;
 	}
 	
 	// 채팅 메세지 db에 저장
 	public int insertChat(WebChat webChat) {
-		SqlSession sqlSession = MyBatisConnection.getConnection();
-
 		try {
 			return sqlSession.insert(ns + "insertChat", webChat);
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
-			MyBatisConnection.close(sqlSession);
+			sqlSession.commit();
 		}
 		return 0;
 	}
 	
 	// 그룹 id에 따라 해당 채팅방에서 주고받은 메세지 리스트로 반환
 	public List<WebChat> chatList(String groupId) {
-		SqlSession sqlSession = MyBatisConnection.getConnection();
-
-		try {
-			return sqlSession.selectList(ns + "chatList", groupId);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			MyBatisConnection.close(sqlSession);
-		}
-
-		return null;
+		
+		return sqlSession.selectList(ns + "chatList", groupId);
+		
 	}
 	
 	// 각 유저의 id가 그룹의 id가 되므로, 관리자가 답변할 유저를 선택할 수 있게 그룹 id를 리스트로 반환
 	public List<String> groupList() {
-		SqlSession sqlSession = MyBatisConnection.getConnection();
 		
-		try {
-			return sqlSession.selectList(ns + "groupList");
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			MyBatisConnection.close(sqlSession);
-		}
+		return sqlSession.selectList(ns + "groupList");
 		
-		return null;
 	}
 }
